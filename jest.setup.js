24641151b6dce975b68967460f6f82537a7bca4f
Ryan Jennings <rybertjen@gmail.com>
2023-10-05 23:19:49 -0700

    fix: unit tests

--- jest.setup.js
+++ jest.setup.js
@@ -2,6 +2,7 @@ import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/asy
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage)
 jest.mock('react-native-vector-icons/FontAwesome', () => 'Icon')
+jest.mock('react-native-vector-icons/FontAwesome5', () => 'Icon')
 jest.mock('redux-persist', () => {
   const real = jest.requireActual('redux-persist')
   return {
